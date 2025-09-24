34314dd64038c0f1d8bb9d9bd19df83b2727269f
yoojua <132237809+yoojua@users.noreply.github.com>
2025-09-25 08:53:26 +0900

    Update 최댓값 만들기 （1）.java

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers); // 오름차순
-        int n = numbers.length;
-        
+        Arrays.sort(numbers); // 오름차순     
         return numbers[n - 1] * numbers[n - 2];
     }
 }
