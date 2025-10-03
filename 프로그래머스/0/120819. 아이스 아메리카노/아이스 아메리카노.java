6254ff847a122634d54d1817ff04e128c014ec04
yoojua <132237809+yoojua@users.noreply.github.com>
2025-10-03 09:38:50 +0900

    Update 아이스 아메리카노.java

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -3,5 +3,7 @@ class Solution {
         int americano = money / 5500;
         int change = money % 5500;
         return new int[]{americano, change};
+        // 아래처럼 줄여서 쓸 수 있다!
+        // return new int[] { money / 5500, money % 5500 };
     }
 }
